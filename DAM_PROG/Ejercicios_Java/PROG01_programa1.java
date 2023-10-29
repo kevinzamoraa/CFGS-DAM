4e5cf831f40260f1115161edd809a74e99f656ba
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-10-29 10:03:33 +0100

    in progress / CFGS DAM 23-24 / Init

---
+++ DAM_PROG/Ejercicios_Java/PROG01_programa1.java
@@ -0,0 +1,15 @@
+/**
+*    La clase HolaMundo implementa una aplicación de consola,
+*    la cual imprime símplemente el siguiente mensaje en pantalla:
+*    'Modulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01' +
+*    'Introducción a la programación' +
+*    'Datos del/de la alumnx' +
+*    'Programa1'
+*/
+
+class HelloWold {
+    public static void main(String[] args) {
+        System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01 \n" + "Introducción a la programación \n" + "Kevin Zamora Amela \n" + "Vinaròs - Castellón \n" + "25/10/2023 \n" + "Programa1");
+    }
+}
+
---
+++ DAM_PROG/Ejercicios_Java/PrimeraAplicNetbeans/src/main/java/kzdesigner/primeraaplicnetbeans/PrimeraAplicNetbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kzdesigner.primeraaplicnetbeans;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class PrimeraAplicNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! \n Primer paso: \n Siguiendo las indicaciones de los contenidos de la unidad, descarga desde los enlaces ofrecidos el IDE  NetBeans e instálalo en tu ordenador. Se recomienda utilizar una versión posterior a la 11.");
+    }
+}
