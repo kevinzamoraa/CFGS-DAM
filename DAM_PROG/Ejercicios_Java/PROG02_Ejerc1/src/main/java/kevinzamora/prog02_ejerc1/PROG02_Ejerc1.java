604acb45854ec8b179b7eb809ff3d2af7951f9bf
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-04 20:54:52 +0100

    in progress / PROG02_Ejerc1

--- DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
@@ -4,6 +4,8 @@
 
 package kevinzamora.prog02_ejerc1;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author kzdesigner
@@ -11,6 +13,38 @@ package kevinzamora.prog02_ejerc1;
 public class PROG02_Ejerc1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!"); // It's default message
+        
+        int valorMaximo = 5000;
+        boolean carnetConducir = false;
+        String yearMonth = "Un mes cualquiera";
+        String nombreYapellidos = "";
+        String[] sexo = {"V", "M"};
+        LocalDate fechaActual = LocalDate.now();
+        LocalDate fechaPasada = LocalDate.of(1970, 01, 01);
+        float saldoCuentaBancaria = 0;
+        float distanciaTierraJupiter = (float) 595.77;
+        
+        System.out.print("Valor máximo: " + valorMaximo);
+        System.out.printf("¿Tiene Carnet de Conducir: ?" + carnetConducir);
+        System.out.print("Mes del año: " + yearMonth);
+        System.out.printf("Introduce un nuevo mes del año: ");
+        yearMonth = System.console().readLine();
+        System.out.printf("Nuevo mes del año: " + yearMonth);
+        System.out.print("Introduce tu Sexo (V/M): ");
+        String sexoIntroducido = System.console().readLine();
+        if (null == sexoIntroducido) {
+            System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
+        } else switch (sexoIntroducido) {
+            case "V":
+                System.out.printf("Tu genero es 'Varón'");
+                break;
+            case "M":
+                System.out.print("Tu genero es 'Mujer'");
+                break;
+            default:
+                System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
+                break;
+        }
     }
 }
