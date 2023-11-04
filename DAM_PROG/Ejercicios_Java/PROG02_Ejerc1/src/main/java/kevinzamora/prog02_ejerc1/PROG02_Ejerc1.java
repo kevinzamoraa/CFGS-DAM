8176aa873ae0a5ed19a8cce7584f38cd305164f1
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-04 19:32:16 +0100

    init / CFGS DAM / PROG02_Ejerc1

---
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kevinzamora.prog02_ejerc1;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class PROG02_Ejerc1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
