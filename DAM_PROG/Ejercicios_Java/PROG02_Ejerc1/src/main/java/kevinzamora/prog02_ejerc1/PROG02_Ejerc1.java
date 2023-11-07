e48e7cf32b107d161fdc966a711edbdd8dde30b1
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-07 13:19:04 +0100

    in progress / CFGS DAM - PROG02_Tarea

--- DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
@@ -5,6 +5,7 @@
 package kevinzamora.prog02_ejerc1;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 /**
  *
@@ -26,25 +27,27 @@ public class PROG02_Ejerc1 {
         float distanciaTierraJupiter = (float) 595.77;
         
         System.out.print("Valor máximo: " + valorMaximo);
-        System.out.printf("¿Tiene Carnet de Conducir: ?" + carnetConducir);
-        System.out.print("Mes del año: " + yearMonth);
-        System.out.printf("Introduce un nuevo mes del año: ");
-        yearMonth = System.console().readLine();
-        System.out.printf("Nuevo mes del año: " + yearMonth);
-        System.out.print("Introduce tu Sexo (V/M): ");
-        String sexoIntroducido = System.console().readLine();
+        System.out.printf("\n ¿Tiene Carnet de Conducir: ?" + carnetConducir);
+        System.out.print("\n Mes del año: " + yearMonth);
+        System.out.printf("\n Introduce un nuevo mes del año: \n");
+        Scanner scanner = new Scanner(System.in);
+        yearMonth = scanner.nextLine();
+        System.out.printf("\n Nuevo mes del año: " + yearMonth);
+        System.out.print("\n Introduce tu Sexo (V/M): ");
+        String sexoIntroducido = scanner.nextLine();
         if (null == sexoIntroducido) {
-            System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
+           System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
         } else switch (sexoIntroducido) {
-            case "V":
-                System.out.printf("Tu genero es 'Varón'");
-                break;
-            case "M":
-                System.out.print("Tu genero es 'Mujer'");
-                break;
-            default:
-                System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
-                break;
+           case "V":
+               System.out.printf("Tu genero es 'Varón'");
+               break;
+           case "M":
+               System.out.print("Tu genero es 'Mujer'");
+               break;
+           default:
+               System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
+               break;
         }
+        // LocalDateTime milisegundosDesdeLaFechaDada = 
     }
 }
