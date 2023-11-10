2e1dd6d95b789820f1e95e52ba1be46883dc8c8d
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-10 14:29:19 +0100

    in progress / CFGS DAM - PROG02 - Tarea

--- DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc1/src/main/java/kevinzamora/prog02_ejerc1/PROG02_Ejerc1.java
@@ -5,6 +5,8 @@
 package kevinzamora.prog02_ejerc1;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
@@ -16,25 +18,86 @@ public class PROG02_Ejerc1 {
     public static void main(String[] args) {
         // System.out.println("Hello World!"); // It's default message
         
+        // Declaración e inicialización de variables principales
         int valorMaximo = 5000;
         boolean carnetConducir = false;
         String yearMonth = "Un mes cualquiera";
-        String nombreYapellidos = "";
+        int numYearMonth;
+        String nombreYapellidos;
         String[] sexo = {"V", "M"};
         LocalDate fechaActual = LocalDate.now();
         LocalDate fechaPasada = LocalDate.of(1970, 01, 01);
-        float saldoCuentaBancaria = 0;
+        float saldoCuentaBancaria = 1;
         float distanciaTierraJupiter = (float) 595.77;
         
-        System.out.print("Valor máximo: " + valorMaximo);
-        System.out.printf("\n ¿Tiene Carnet de Conducir: ?" + carnetConducir);
-        System.out.print("¿Quieres cambiar tu respues? ¿Tienes 'carnet'?");
-        System.out.print("\n Mes del año: " + yearMonth);
-        System.out.printf("\n Introduce un nuevo mes del año: \n");
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Valor máximo: " + valorMaximo);
+        System.out.print("¿Tienes Carnet de Conducir? (Introduce 'Y' para decir SÍ o 'N' para decir NO)");
+        String tieneCarnet = scanner.nextLine();
+        switch (tieneCarnet) {
+            case "Y":
+                System.out.print("El/La empleadx tiene carnet de conducir \n");
+                break;
+            case "N":
+                System.out.printf("El/La trabajador/a NO tiene carnet de conducir \n");
+                break;
+            default:
+                System.out.print("El valor introducido no es válido \n");
+        }
+        
+        System.out.print("\n Mes del año por defecto: " + yearMonth);
+        System.out.printf("\n Introduce un mes del año (formato: texto): \n");
         yearMonth = scanner.nextLine();
-
-        System.out.printf("\n Nuevo mes del año: " + yearMonth);
+        System.out.print("Has introducido el més:" + yearMonth);
+        System.out.printf("\n Introduce el mismo mes del año (formato: numérico): \n");
+        numYearMonth = scanner.nextInt();
+        switch (numYearMonth) {
+            case 1:
+                System.out.print("Has elegido el número referente a ENERO");
+                break;
+            case 2:
+                System.out.print("Has elegido el número referente a FEBRERO");
+                break;
+            case 3:
+                System.out.print("Has elegido el número referente a MARZO");
+                break;
+            case 4:
+                System.out.print("Has elegido el número referente a ABRIL");
+                break;
+            case 5:
+                System.out.print("Has elegido el número referente a MAYO");
+                break;
+            case 6:
+                System.out.print("Has elegido el número referente a JUNIO");
+                break;
+            case 7:
+                System.out.print("Has elegido el número referente a JULIO");
+                break;
+            case 8:
+                System.out.print("Has elegido el número referente a AGOSTO");
+                break;
+            case 9:
+                System.out.print("Has elegido el número referente a SEPTIEMBRE");
+                break;
+            case 10:
+                System.out.print("Has elegido el número referente a OCTUBRE");
+                break;
+            case 11:
+                System.out.print("Has elegido el número referente a NOVIEMBRE");
+                break;
+            case 12:
+                System.out.print("Has elegido el número referente a DICIEMBRE");
+                break;
+            default:
+                System.out.print("El número introducido no corresponde a ningún mes de nuestro calendario");
+                break;
+        }
+        
+        System.out.print("\n Introduce un nombre y apellidos: ");
+        nombreYapellidos = scanner.nextLine();
+        System.out.println("Nombre introducido: " + nombreYapellidos);
+        
         System.out.print("\n Introduce tu Sexo (V/M): ");
         String sexoIntroducido = scanner.nextLine();
         switch (sexoIntroducido) {
@@ -48,6 +111,12 @@ public class PROG02_Ejerc1 {
                System.out.printf("El sexo introducido no se encuentra en nuestra base de datos");
                break;
         }
-        // LocalDateTime milisegundosDesdeLaFechaDada = 
+        
+        System.out.print("\n Fecha actual: " + fechaActual);
+        System.out.print("\n Fecha de referéncia: " + fechaPasada.format(DateTimeFormatter.ISO_DATE));
+        
+        // TODO / To Fix - Only obtains the years of diference between both
+        int milisegundosDesdeLaFechaDada = fechaActual.compareTo(fechaPasada);
+        System.out.println("Han pasado " + milisegundosDesdeLaFechaDada + "milisegundos entre ambas fechas");
     }
 }
