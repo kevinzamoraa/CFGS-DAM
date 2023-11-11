e82039f9a3a16bc0c160bf2a3c49b3620217a56e
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-11 23:17:12 +0100

    in progress / CFGS DAM - PROG02 - Ej5

---
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc5/src/main/java/kevinzamora/prog02_ejerc5/PROG02_Ejerc5.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kevinzamora.prog02_ejerc5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class PROG02_Ejerc5 {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Introduce un número de segundos:");
+        int segundosIntroducidos = scanner.nextInt();
+        
+        if (segundosIntroducidos >= 60) {
+            segundosIntroducidos = segundosIntroducidos/60;
+            System.out.print(segundosIntroducidos + " minutos \n");
+            if (segundosIntroducidos >= 60) {
+                segundosIntroducidos = segundosIntroducidos/60;
+                System.out.print(segundosIntroducidos + " horas \n");
+                if (segundosIntroducidos >= 60) {
+                    segundosIntroducidos = segundosIntroducidos/60;
+                    System.out.print(segundosIntroducidos + " días \n");
+                }
+            }
+        }
+    }
+}
