95952798ede92024ad395f4e5822553c35e18df6
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-12 11:33:38 +0100

    fixed feature / Adding missing feature / CFGS DAM - PROG02 - Ej7

--- DAM_PROG/Ejercicios_Java/PROG02_Ejerc7/src/main/java/kevinzamora/prog02_ejerc7/PROG02_Ejerc7.java
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc7/src/main/java/kevinzamora/prog02_ejerc7/PROG02_Ejerc7.java
@@ -4,6 +4,7 @@
 
 package kevinzamora.prog02_ejerc7;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -18,11 +19,17 @@ public class PROG02_Ejerc7 {
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Introduce el valor del índice C1 \n");
-        int C1 = scanner.nextInt();
-        System.out.print("\n Introduce el valor del índice C2 \n");
-        int C2 = scanner.nextInt();
+        float C1 = scanner.nextFloat();
         
-        float resultado = (C2*-1)/C1;
-        System.out.print("\n El resultado de la ecuación de primer grado, entre los dos índices introducidos, es: " + resultado);
+        System.out.print("\n Introduce el valor del índice C2 \n");
+        float C2 = scanner.nextFloat();
+        
+        double resultado = (C2*-1)/C1;
+        System.out.println("\n Resultado antes de aplicar el patrón de 4 decimales: " + resultado);
+        
+        DecimalFormat dfResult = new DecimalFormat("##.####");
+        String formatedResult = dfResult.format(resultado);
+        
+        System.out.print("\n El resultado de la ecuación de primer grado, entre los dos índices introducidos, es: " + formatedResult);
     }
 }
