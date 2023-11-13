8d2e70ccf2fd83ac020eef21aa506f29ae126de5
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-11-13 12:53:35 +0100

    feature / CFGS DAM - PROG02 - Ej10

--- DAM_PROG/Ejercicios_Java/PROG02_Ejerc10/src/main/java/kevinzamora/prog02_ejerc10/PROG02_Ejerc10.java
+++ DAM_PROG/Ejercicios_Java/PROG02_Ejerc10/src/main/java/kevinzamora/prog02_ejerc10/PROG02_Ejerc10.java
@@ -30,24 +30,50 @@ public class PROG02_Ejerc10 {
         short sx = 5;
         short sy = 2;
         short sz = (short) (sx-sy);
-        sx = 32767;
-        sy = 1;
-        sz = (short) (sx+sy);
+        
         char cx = '\u000F';
         char cy = '\u0001';
         int z = cx-cy;
+        
+        
+        // Programa para Vista en Consola
+        
+        System.out.println("\n ---- Conversiones entre enteros y coma flotante ----");
+        System.out.println("Producto de int por float: j= i*x = " + j);
+        System.out.println("Producto de float por double: dz=dx * y = " + dz);
+        
+        System.out.println("\n ---- Operaciones con byte ----");
+        System.out.println("byte: 5 - 2 = " + bz);
+        System.out.println("byte -128 - 1 = " + (byte)(bx - 1));
+        System.out.println("(int)(-128 - 1) = " + (bx - by));
+        
+        System.out.println("\n ---- Operaciones con short ----");
+        System.out.println("short: 10 - 1 = " + sz);
+        // Variables recolocadas para ajustarse al orden de las operaciones y en base a la obtención de la respuesta requerida
+        sx = 32767;
+        sy = 1;
+        sz = (short) (sx+sy);
+        System.out.println("short 32767 + 1 = " + sz);
+        
+        System.out.println("\n ---- Operaciones con char ----");
+        System.out.println("char: - = " + z);
+        // Variables recolocadas para ajustarse al orden de las operaciones y en base a la obtención de la respuesta requerida
         z = cx-1;
         cx = '\uFFFF';
+        System.out.println("char(0x000f) - 1 = " + z);
         z = cx;
+        System.out.println("(int)() = " + z);
         sx = (short) cx;
+        
         sx = -32768;
         cx = (char) sx;
         z = sx;
         sx = -1;
+        System.out.println("(short)() = " + sx);
         cx = (char) sx;
+        System.out.println("-32768 short-char-int = " + z*sx);
         z = cx;
-        
-        
+        System.out.println("-1 short-char-int = " + z);
         
     }
 }
