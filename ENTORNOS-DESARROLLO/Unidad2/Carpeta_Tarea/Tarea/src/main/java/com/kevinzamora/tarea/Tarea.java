f5ce00bd45777eaeec2bd5c9564da6f97631d8bf
Kevin Zamora <56316102+zamelkev@users.noreply.github.com>
2023-12-07 16:57:32 +0100

    Add files via upload

---
+++ ENTORNOS-DESARROLLO/Unidad2/Carpeta_Tarea/Tarea/src/main/java/com/kevinzamora/tarea/Tarea.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.tarea;
+
+/**
+ *
+ * @author Kanonimus
+ */
+public class Tarea {
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+}
