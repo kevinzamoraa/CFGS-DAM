17031afe14273b60cbe9473fb73032130912d2a8
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-01-05 09:35:28 +0100

    feature / CFGS DAM - PROG04 - Tarea - Ej.4

--- DAM_PROG/Ejercicios_Java/PROG04_Ejerc4/src/main/java/kevinzamora/prog04_ejerc4/PROG04_Ejerc4.java
+++ DAM_PROG/Ejercicios_Java/PROG04_Ejerc4/src/main/java/kevinzamora/prog04_ejerc4/PROG04_Ejerc4.java
@@ -12,17 +12,21 @@ import java.util.Scanner;
  */
 public class PROG04_Ejerc4 {
 
+    public int numInt = 5;
+    public int numMax = 10;
+    public int numIntReq = 0;
+    
     public static void main(String[] args) {
         
-        loadMainPage();
+        PROG04_Ejerc4 myClass = new PROG04_Ejerc4();
+        myClass.loadMainPage();
         
     }
 
-    public static void loadMainPage() {
+    public void loadMainPage() {
         
         Scanner scanner = new Scanner(System.in);
-        int numInt = 5;
-        int numMax = 0;
+        
         
         System.out.println("\n BIENVENID@S AL JUEGO 'ADIVINA QUÉ PIENSO' \n");
         System.out.println("¿¡EMPEZAMOS EL JUEGO!? \n");
@@ -47,25 +51,37 @@ public class PROG04_Ejerc4 {
                 int numOculto = (int)Math.floor(Math.random()*numMax+1); 
                 //genera un número aleatorio entre 0 y el número máximo introducido, ambos incluidos.
                 System.out.println("EL NÚMERO OCULTO HA SIDO GENERADO CON ÉXITO... ¿ACEPTAS EL DESAFIO? \n");
-                System.out.println("NÚMERO DE INTENTOS: " + numInt);
-                
+                System.out.println("NÚMERO DE INTENTOS: " + numInt + "\n");
+                             
                 int numIntroducido = 0;
-                while (numOculto == numIntroducido ) {
+                while (numOculto != numIntroducido ) {
                     System.out.println("INTRODUCE UN NÚMERO CANDIDATO (Nº ENTERO):");
                     numIntroducido = scanner.nextInt();
+                    numInt--;
+                    numIntReq++;
                     if (numIntroducido == numOculto) { 
-                        System.out.println("¡¡¡ENHORABUENA!!! HAS ACERTADO EL NÚMERO OCULTO \n"); 
+                        System.out.println("¡¡¡HAS GANADO!!! HAS NECESITADO " + numIntReq + " INTENTOS \n"); 
                         System.out.println("THE END");
                         loadMainPage();
                         break;
+                    } else if (numInt <= 0) {
+                        System.out.println("¡PERDISTE! INTENTOS CONSUMIDOS");
+                        loadMainPage();
+                        break;
                     } else {
-                        System.out.println("NO HA HABIDO SUERTE...\n ¡MUCHA SUERTE!\n PRUEBA OTRA VEZ");
+                        System.out.println("NO HA HABIDO SUERTE...\n ¡MUCHA SUERTE!\n Y PRUEBA OTRA VEZ");
+                        if (numOculto < numIntroducido) {
+                            System.out.println("EL NÚMERO OCULTO ES MENOR \n");
+                        }
                     }
                 } 
                 break;
             case 3:
                 System.out.println("HAS SELECCIONADO LA OPCIÓN 3: SALIR");
+                System.out.println("¡¡¡HASTA PRONTO!!!");
                 break;
+            default:
+                System.out.println("OPCIÓN INCORRECTA. HASTA PRONTO! ");
         }
         
     }
