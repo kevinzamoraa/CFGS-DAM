5126d3fcc95db440b9762d016198ca9c527cffdc
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-03-16 23:30:55 +0100

    Completed / CFGS DAM - PROG07 - Tarea

--- 1-1er_Curso/DAM_PROG/Ejercicios_Java/PROG07_Tarea/src/main/java/kevinzamora/prog07_tarea/Controler/Imprimible.java
+++ 1-1er_Curso/DAM_PROG/Ejercicios_Java/PROG07_Tarea/src/main/java/kevinzamora/prog07_tarea/Controler/Imprimible.java
@@ -1,3 +1,7 @@
+/* No he conseguido organizar mis ideas como para lograr implementar correctamente 
+la interfaz solicitada. Según tengo entendido, las interfaces sirven para definir entidades o 
+para encapsular el uso de los métodos de una o varias clases, dificultando así su acceso indeseado */
+
 ///*
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
