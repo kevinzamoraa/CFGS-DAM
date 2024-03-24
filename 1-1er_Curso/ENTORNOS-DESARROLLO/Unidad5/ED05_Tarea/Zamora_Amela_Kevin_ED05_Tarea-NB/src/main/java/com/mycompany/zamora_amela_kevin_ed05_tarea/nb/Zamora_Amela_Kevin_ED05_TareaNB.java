bb70149e844a1e83eb566c4ae97cc25681e57d04
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-03-24 11:03:09 +0100

    completed / CFGS DAM - ED05 - Tarea

---
+++ 1-1er_Curso/ENTORNOS-DESARROLLO/Unidad5/ED05_Tarea/Zamora_Amela_Kevin_ED05_Tarea-NB/src/main/java/com/mycompany/zamora_amela_kevin_ed05_tarea/nb/Zamora_Amela_Kevin_ED05_TareaNB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zamora_amela_kevin_ed05_tarea.nb;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class Zamora_Amela_Kevin_ED05_TareaNB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
