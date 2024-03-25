c4a2780b74f152939be458e13a81b77c92fb907f
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-03-25 17:18:05 +0100

    sorted repo

--- 1-1er_Curso/DAM_BD-Bases_Datos/Unidad6/zamora_amela_kevin_BD06_Tarea.sql
+++ 1-1er_Curso/DAM_BD-Bases_Datos/Unidad6/zamora_amela_kevin_BD06_Tarea.sql
@@ -61,7 +61,70 @@ EXCEPTION
 END;
 /
 
+/* Intento de comprobacion -> Al ejecutar la función desde un bloque anonimo, no 
+   nuestra ningún mensaje de error y en consola comenta 'copilado con errores' */
 BEGIN
-    CambiarCategoria(11111,11111);
+    CambiarCategoria(111111,111111);
+END;
+/
+
+// Actividad 2
+CREATE OR REPLACE TRIGGER controlar_variables
+BEFORE INSERT OR UPDATE ON productos FOR EACH ROW
+BEGIN
+
+    SELECT cod_producto,
+        CASE
+            WHEN LENGTH(cod_producto) > 10 THEN 'La clave de producto no puede contener 
+                más de 10 carácteres'
+/*                RAISE_APPLICATION_ERROR(-20001, 'La clave de producto no puede contener 
+                más de 10 carácteres'); */
+            END AS cod_producto
+    FROM productos;
+    
+    SELECT iva,
+        CASE
+            WHEN iva not in (4,10,21) THEN
+                 RAISE_APPLICATION_ERROR(-20002, 'El valor de IVA introducido no es válido');
+    FROM productos;
+    
+    SELECT iva
+        CASE
+            WHEN iva is null THEN
+                 RAISE_APPLICATION_ERROR(-20003, 'El valor de IVA nunca puede estar vacio');
+    FROM productos;
+    
+    SELECT precioOferta, precio
+        CASE
+            WHEN precioOferta > precio THEN
+                RAISE_APPLICATION_ERROR(-20004, 'El precio en oferta debe ser inferior al 
+                precio original/previo');
+    FROM productos;
+    
+    SELECT cod_categoria, precioOferta, iva
+        CASE
+            WHEN cod_categoria > 50 and precioOferta is not null and iva != 21 THEN 
+                RAISE_APPLICATION_ERROR(-20005, 'El producto no puede tener un precio en 
+                oferta y debe tener un IVA del 21');
+                precioOferta := null;
+                iva := 21;
+    FROM productos;
+    
+    SELECT cod_categoria, precioOferta, peso, iva
+        CASE
+            WHEN cod_categoria <= 50 and precioOferta is null and peso > 30 and 
+            (iva != 4 or iva != 10) THEN 
+                 RAISE_APPLICATION_ERROR(-20006, 'El producto puede tener un precio en 
+                oferta, a definir, su IVA puede ser del 4 o 10 porciento y su peso debe 
+                ser inferior a 30');
+    FROM productos;
+    
+    SELECT cod_categoría, precio
+        CASE
+            WHEN cod_categoria is null OR precio is null THEN
+                 RAISE_APPLICATION_ERROR(-20007, 'Todos los productos deben pertenecer a una 
+                categoría y también deben tener un precio de venta');    
+    FROM productos;
+    
 END;
 /
