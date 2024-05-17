51246466e70c46d652e85c559d176a737ff14104
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-05-17 17:08:40 +0200

    in progress / solving PROG 2nd exam practical exercise / init base project

---
+++ 1-1er_Curso/PROG/Examen-2T/PARCIAL2_TareaMod/src/main/java/com/kevinzamora/parcial2_tarea/PARCIAL2_Tarea.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.parcial2_tarea;
+
+import vista.Calculadora;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class PARCIAL2_Tarea {
+
+    public static void main(String[] args) {
+        
+        Calculadora mainMenuObj = new Calculadora();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Calculadora().setVisible(true);
+            }
+        });
+    }
+        
+}
