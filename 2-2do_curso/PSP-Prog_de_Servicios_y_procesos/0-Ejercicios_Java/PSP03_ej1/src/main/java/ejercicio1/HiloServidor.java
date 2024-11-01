c2844524f733ae4c7b1b2681bb4530e2a7d93a28
Kevin Zamora <56316102+kevinzamoraa@users.noreply.github.com>
2024-11-01 14:44:12 +0100

    in progress / 2º CFGS DAM - PSP03 - Tarea / Ej. 1 y 2 / adding comments

--- 2-2do_curso/PSP-Prog_de_Servicios_y_procesos/0-Ejercicios_Java/PSP03_ej1/src/main/java/ejercicio1/HiloServidor.java
+++ 2-2do_curso/PSP-Prog_de_Servicios_y_procesos/0-Ejercicios_Java/PSP03_ej1/src/main/java/ejercicio1/HiloServidor.java
@@ -12,39 +12,68 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * 
+ * Clase/entidad HiloServidor que extiende a la clase nativa 'Hilo'
  * @author kzdesigner
  */
 public class HiloServidor extends Thread {
 
+    /** 
+     * Definimos el zócalo/'socket' de conexión
+     */
     private Socket sc;
     
+    /**
+     * Creamos el construcctor de clase a utilizar y definimos que le podamos 
+     * pasar el 'zócalo' como parámetro.
+     * @param sc Zócalo de conexión (o Socket)
+     */
     public HiloServidor(Socket sc) {
         this.sc = sc;
     }
     
+    /** 
+     * Método 'run()' encargado de conectar el cliente y el servidor y también 
+     * de realizar todas , las operaciones de entrada, salida y comparación 
+     * necesarias.
+     */
     public void run() {
     
+        /*
+        Imprimimos un mensaje informativo y creamos e inicializamos los 
+        flujos de entrada y de salida.
+        */
         System.out.println("Cliente conectado");
         DataInputStream in = null;
         DataOutputStream out = null;
+        // Definimos un método de control de excepciones 'try catch' para 
+        // capturar los posibles errores durante la ejecución del programa
         try {
+            // Leemos y asignamos los datos recibidos desde los 'flujos de entrada
+            // y salida y los guardamos en las variables 'in' y 'out'
             in = new DataInputStream(sc.getInputStream());
             out = new DataOutputStream(sc.getOutputStream());
             
+            // Generamos el número aleatorio y lo guardamos en una variable
             int numAleatorio = generarNumeroAleatorio(1,100);
+            // Inicializamos la variable 'numUsuario' a cero
             int numUsuario = 0;
             
+            // Imprimimos el número generado
             System.out.println("Num generado: " + numAleatorio);
             
+            // Creamos un bucle 'haz mientras'
             do {
                 
                 out.writeUTF("Escribe un numero entre 1 y 100");
-
+                // Imprimimos un mensaje y leemos el 'entero' introducido 
+                // por teclado
                 numUsuario = in.readInt();
-
+                
+                // Imprimimos el número recibido
                 System.out.println("Numero recibido: " + numUsuario);
-
+                
+                // Realizamos la comparación para saber si es correcto
                 if(numUsuario == numAleatorio){
                     out.writeUTF("¡Has ganado!");
                 }else if (numUsuario < numAleatorio){
@@ -52,20 +81,22 @@ public class HiloServidor extends Thread {
                 }else{
                     out.writeUTF("El numero buscado es menor");
                 }
-
+                // Evaluamos sí el número del usuario y el generado son iguales
                 out.writeBoolean(numUsuario == numAleatorio);
             } while(numUsuario != numAleatorio);
             
+            // Cerramos la conexión e imprimimos el mensaje correspondiente
             sc.close();
             System.out.println("Cliente desconectado");
             
-        } catch (IOException ex) {
+        } catch (IOException ex) { // Capturamos los errores y los guardamos en un 'logger'
             Logger.getLogger(HiloServidor.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             try {
+                // Cerramos los flujos de entrada y de salida
                 in.close();
                 out.close();
-            } catch (IOException ex) {
+            } catch (IOException ex) { // Capturamos los posibles errores en un logger
                 Logger.getLogger(HiloServidor.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
