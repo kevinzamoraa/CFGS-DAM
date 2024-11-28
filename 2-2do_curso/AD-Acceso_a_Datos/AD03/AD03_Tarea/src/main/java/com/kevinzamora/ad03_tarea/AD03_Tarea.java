2cc62ae1a0caacea100459c10b2d5515035a27db
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-11-28 18:38:59 +0100

    init task / 2o CFGS DAM / AD03 - Tarea

---
+++ 2-2do_curso/AD-Acceso_a_Datos/AD03/AD03_Tarea/src/main/java/com/kevinzamora/ad03_tarea/AD03_Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.ad03_tarea;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class AD03_Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
