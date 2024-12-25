24c15b2ccf6d0d3fd7e6457f0248e6c228ec1fc8
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-12-25 11:08:00 +0100

    init task / CFGS DAM - DI01 - Tarea / Init NetBeans Project

---
+++ 2-2do_curso/DI-Desarrollo_Interfaces/DI01-Tarea/src/main/java/com/kevinzamora/di01/tarea/DI01Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.di01.tarea;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class DI01Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
