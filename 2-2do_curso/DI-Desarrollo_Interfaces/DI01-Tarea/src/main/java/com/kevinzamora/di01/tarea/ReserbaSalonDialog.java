e668005e02386a267599564e9167a4de816bf624
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-12-27 23:55:50 +0100

    in progress / CFGS DAM - DI01 - Tarea

--- 2-2do_curso/DI-Desarrollo_Interfaces/DI01-Tarea/src/main/java/com/kevinzamora/di01/tarea/ReserbaSalonDialog.java
+++ 2-2do_curso/DI-Desarrollo_Interfaces/DI01-Tarea/src/main/java/com/kevinzamora/di01/tarea/ReserbaSalonDialog.java
@@ -53,7 +53,7 @@ public class ReserbaSalonDialog extends javax.swing.JDialog {
 
         tipoCocina.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Bufé", "Carta", "Pedir cita con el Chef", "No precisa" }));
 
-        necesitaHabitaciones.setText("Se precisan habitaciones");
+        necesitaHabitaciones.setLabel("Necesita habitaciones");
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -89,7 +89,7 @@ public class ReserbaSalonDialog extends javax.swing.JDialog {
                         .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(detallesReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
                         .addComponent(scrollPaneReservas, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
         layout.setVerticalGroup(
