2b267eb58aa85ea3a0b0dacf019327f52759c650
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-01-05 21:13:45 +0100

    init task / CFGS DAM / AD04 - Tarea

---
+++ 2-2do_curso/AD-Acceso_a_Datos/AD04/HibernateMysql/src/main/java/com/kevinzamora/hibernatemysql/HibernateMysql.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.hibernatemysql;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class HibernateMysql {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
