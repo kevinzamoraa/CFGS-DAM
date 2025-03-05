1abd3153f9fb59d291ff600e0b2e1e1601c9fc4e
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-03-05 13:02:01 +0100

    upload files / 2o CFGS DAM / PSP06 - Tarea / Init Project Structure

---
+++ 2-2do_curso/PSP-Prog_de_Servicios_y_procesos/0-Ejercicios_Java/PSP06-Tarea/src/main/java/com/kevinzamora/psp06_tarea/PSP06Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kevinzamora.psp06_tarea;
+
+/**
+ *
+ * @author kzdesigner
+ */
+public class PSP06Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
