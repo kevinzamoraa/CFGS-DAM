e4f0d1cafd1626278ed92ba4cce02da52a7ca849
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-04-12 00:19:52 +0200

    in progress / 2o FPGS DAM / AD07 - Tarea / solving issues #1

---
+++ 2-2do_curso/AD-Acceso_a_Datos/AD07/prueba-sensor/src/main/java/com/ejemplos/prueba/PruebaSensor.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejemplos.prueba;
+import com.kevinzamora.ad07_tarea.SensorTemperatura;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ *
+ * @author kzdesigner
+ */
+
+
+
+public class PruebaSensor {
+    public static void main(String[] args) {
+        // Crear instancia del sensor
+        SensorTemperatura sensor = new SensorTemperatura();
+        
+        // Registrar listener para eventos
+        sensor.addPropertyChangeListener((PropertyChangeEvent evt) -> {
+            System.out.println("Cambio detectado:");
+            System.out.println("Propiedad: " + evt.getPropertyName());
+            System.out.println("Valor anterior: " + evt.getOldValue());
+            System.out.println("Nuevo valor: " + evt.getNewValue());
+        });
+        
+        // Probar el sensor
+        System.out.println("Probando el sensor...");
+        sensor.setTemperatura(25.5);
+        sensor.setEstadoActivo(true);
+        System.out.println("Temperatura actual: " + sensor.getTemperatura());
+        System.out.println("Estado actual: " + sensor.getEstadoActivo());
+        
+        // Probar el método de cálculo
+        double promedio = sensor.calcularPromedioTemperatura(5);
+        System.out.println("Promedio de temperatura: " + promedio);
+    }
+}
