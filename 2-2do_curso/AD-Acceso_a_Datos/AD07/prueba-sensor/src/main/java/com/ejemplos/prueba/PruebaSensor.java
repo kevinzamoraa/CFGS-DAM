eafe0f57f72ddfc4d0299766a795e193b67f5673
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-04-12 21:38:13 +0200

    in progress / 2o FPGS DAM / AD07 - Tarea

--- 2-2do_curso/AD-Acceso_a_Datos/AD07/prueba-sensor/src/main/java/com/ejemplos/prueba/PruebaSensor.java
+++ 2-2do_curso/AD-Acceso_a_Datos/AD07/prueba-sensor/src/main/java/com/ejemplos/prueba/PruebaSensor.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.ejemplos.prueba;
-import com.kevinzamora.ad07_tarea.SensorTemperatura;
+import com.ejemplos.prueba.SensorTemperatura;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
--- 2-2do_curso/AD-Acceso_a_Datos/AD07/prueba-sensor/src/main/java/com/ejemplos/prueba/SensorTemperatura.java
+++ 2-2do_curso/AD-Acceso_a_Datos/AD07/prueba-sensor/src/main/java/com/ejemplos/prueba/SensorTemperatura.java
@@ -1,8 +1,10 @@
+package com.ejemplos.prueba;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.kevinzamora.ad07_tarea;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.Serializable;
