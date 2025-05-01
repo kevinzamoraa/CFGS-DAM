a06309429d27975b5b8e24a3f13e946e234b32cd
Kevin Zamora <kevin.zamora.amela@gmail.com>
2025-05-01 21:11:05 +0200

    feature / 2o CFGS DAM / SGE05 - Tarea

--- 2-2do_curso/SGE-Sistemas_de_Gestion_Empresarial/U05/gestion_usuarios/__manifest__.py
+++ 2-2do_curso/SGE-Sistemas_de_Gestion_Empresarial/U05/gestion_usuarios/__manifest__.py
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
+
 {
     'name': 'Gestión de Usuarios',
     'version': '1.0',
-    'author': 'Kevin Zamora Amela',
+    'author': 'Tu Nombre',
     'category': 'Herramientas',
     'summary': 'Módulo para gestionar usuarios personalizados',
     'depends': ['base'],
-    'data': ['views/user_views.xml'],
+    'data': [
+        'views/user_views.xml',
+    ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
-=======
-{
-    'name': 'Gestión de Usuarios',
-    'version': '1.0',
-    'author': 'Kevin Zamora Amela',
-    'category': 'Herramientas',
-    'summary': 'Módulo para gestionar usuarios personalizados',
-    'depends': ['base'],
-    'data': ['views/user_views.xml'],
-    'installable': True,
-    'application': True,
-}
->>>>>>> fc71c9d72898a25225fc78625a06e724eda4e53c
